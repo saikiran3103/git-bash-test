23194fb5f8dbd4387518b62073a98b16fc9fa17d
saikiran3103 <saikiran31057@gmail.com>
2017-11-18 15:49:19 -0600

    java folder

--- java/CallingMethodsInSameClass.java
+++ java/CallingMethodsInSameClass.java
@@ -14,6 +14,7 @@ public class CallingMethodsInSameClass
 
 	public static void printOne() {
 		System.out.println("Hello World");
+		System.out.println("Hello World rep1");
 	}
 
 	public static void printTwo() {

2b3b15c6d3d9a7f6e40eea0adf6f9d2ff0c400ed
saikiran3103 <saikiran31057@gmail.com>
2017-11-18 15:45:26 -0600

    clone rep push

--- java/CallingMethodsInSameClass.java
+++ java/CallingMethodsInSameClass.java
@@ -14,6 +14,8 @@ public class CallingMethodsInSameClass
 
 	public static void printOne() {
 		System.out.println("Hello World");
+		System.out.println("Hello World2");
+		
 	}
 
 	public static void printTwo() {
